f5606f1f63aff543c5939ed4c8cc7809e5ea4c3d
Sergiolm17 <kingsergiolm17@gmail.com>
2020-06-02 18:35:22 -0500

    install api

--- server.js
+++ server.js
@@ -2,7 +2,46 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
+app.post("/", async (req, res) => {
+  var randlangs = shuffle(Object.keys(languages));
+  try {
+    const rute = [];
+    console.log(req.body);
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+    var { text, number } = req.body;
+    var retutext = { text };
+
+    for (let index = 0; index < number + 1; index++) {
+      to = index === number ? "es" : randlangs.next().value;
+      retutext = await translate(retutext.text, {
+        to,
+      });
+      let fix_esp = await translate(retutext.text, {
+        to: "es",
+      });
+      rute.push({
+        from: languages[retutext.from.language.iso],
+        to: languages[to],
+        fix_esp: fix_esp.text,
+        text: retutext.text,
+      });
+    }
+    res.send({
+      input: text,
+      output: rute[rute.length - 1].text || null,
+      rute,
+      erro: "",
+    });
+  } catch (err) {
+    res.send({ text: "", rute, err });
+  }
 });
+function* shuffle(array) {
+  let i = array.length;
+
+  while (i--) {
+    yield array.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+  }
+}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Example app listening on port 3000!"));
